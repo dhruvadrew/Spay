c5f02e558e6ee9c2065f53960a0e25dde80740de
Dhruva Barua <db466@duke.edu>
2024-11-02 19:05:07 -0400

    new website

--- spay-app/src/App.js
+++ spay-app/src/App.js
@@ -1,15 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './Navbar';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./Navbar/Navbar";
+import NavbarHook from "./NavbarHook/NavbarHook";
+import Home from "./pages/Home";
+import News from "./pages/News";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="Spay">
-        <Navbar></Navbar>
-      </header>
-    </div>
+    <Router>
+      {/* <Navbar /> */}
+      <NavbarHook />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/news" element={<News />} />
+          {/* Define other routes that you need*/}
+        </Routes>
+      </main>
+    </Router>
   );
-}
+};
 
 export default App;
