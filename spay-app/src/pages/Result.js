7d08ec1bf1112933887acac7272df49c0b471ccb
Dhruva Barua <db466@duke.edu>
2024-11-03 01:35:37 -0400

    results stuff

--- spay-app/src/pages/Result.js
+++ spay-app/src/pages/Result.js
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import ProductRow from '../components/ProductRow'
 
 function Result() {
+
+    useEffect(() => {
+        getData();
+    }, []);
+
+    const getData = async () => {
+        const response = await fetch('https://fakestoreapi.com/products');
+        const data = await response.json();
+        console.log(data);
+    }
+
   return (
     <div>
         <ProductRow 
@@ -9,6 +20,8 @@ function Result() {
         name="Vineego 50-in Modern Gray Chenille Sectional" 
         price="$29.99" 
         />
+
+
 
     </div>
   )
